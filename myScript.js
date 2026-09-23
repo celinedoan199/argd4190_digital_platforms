898ffacd83103716dc68e3c43a8d13f7174e24d2
celinedoan199 <celinedoan199@gmail.com>
2026-09-22 20:38:41 -0400

    Cool And Epic Horse Page

--- myScript.js
+++ myScript.js
@@ -18,33 +18,122 @@ function checkAnswer() {
 
   if (userInput === correctAnswer) {
     console.log('Correct!');
-    feedbackElement.textContent = "CORRECT!! CONGRATS!!!.";
-    feedbackElement.style.color = "green"
+    feedbackElement.textContent = "CORRECT!! CONGRATS!!!";
+    feedbackElement.style.color = "white"
+    feedbackElement.style.background = "green"
+    feedbackElement.style.width = "200px"
+    feedbackElement.style.textAlign = "center"
+
   } else {
     console.log('Wrong!');
     feedbackElement.textContent = "WRONG. TRY AGAIN.";
-    feedbackElement.style.color = "red"
+    feedbackElement.style.color = "white"
+    feedbackElement.style.background = "red"
+    feedbackElement.style.width = "200px"
+    feedbackElement.style.textAlign = "center"
   }
 }
 const feedbackElement = document.getElementById("feedback");
 
-// Circle Color //
-let circle = document.getElementById("bgCircle")
-let circColor = document.getElementById("#circColor")
-let circColorOutp = document.querySelector("output[for-circColor]")
+// Very Important Function //
+function updateOutput(element, input) {
+  element.textContent = input.value
+}
+// Very Important Function //
 
-function updColor(element, colorInput) {
+// Circle Color //
+let circle = document.getElementById("circle")
+let circColorOutput = document.querySelector("output[for=circColor]")
+let circColor = document.getElementById("circColor") //input//
+
+function changeColor(element, colorInput) {
   element.style.backgroundColor = colorInput.value
 }
-circColor.addEventListener("input", function(){
-  updColor(circle, circColor.value)
+circColor.addEventListener("input", function () {
+  changeColor(circle, circColor)
+  updateOutput(circColorOutput, circColor)
 })
-function outputValue(element, value) {
-  element.textContent = value
+
+// Scale It (circle)//
+let circScaleOut = document.querySelector("output[for=circScale]")
+let circScale = document.getElementById("circScale") //input//
+ 
+function changeScale(element, scaleInput) {
+  element.style.scale = scaleInput.value
 }
-// update circle color //
-circColor.addEventListener("input", function(){
-  updColor(circle, circColor.value)
-  updateOutput(circColorOutp, circColor.value)
+circScale.addEventListener("input", function (event) {
+  changeScale(circle, circScale)
+  updateOutput(circScaleOut, circScale)
 })
-outputValue(circColorOutp), circColor.value
+
+// Scale Him //
+let golImg = document.getElementById("golshi")
+let golScaleOut = document.querySelector("output[for=golScale]")
+let golScale = document.getElementById("golScale")
+ 
+function changeScale(element, scaleInput) {
+  element.style.scale = scaleInput.value
+}
+golScale.addEventListener("input", function (event) {
+  changeScale(golImg, golScale)
+  updateOutput(golScaleOut, golScale)
+})
+
+// Jumping //
+let input = document.getElementById("wordInput")
+let image = document.getElementById("golshi");
+
+wordInput.addEventListener('input', function(){
+  image.classList.add('jump')
+  setTimeout(function(){
+    image.classList.remove('jump')
+  }, 200)
+})
+
+// Border Weight //
+let border = document.getElementById("imgContainer")
+let borderWeightOutput = document.querySelector("output[for=borderWeight]")
+let borderWeight = document.getElementById("borderWeight") //input//
+
+function updBorder(element, borderWeightInput){
+  element.style.borderWidth = borderWeightInput.value + "px"
+}
+
+borderWeight.addEventListener("input", function () {
+  updBorder(border, borderWeight)
+  updateOutput(borderWeightOutput, borderWeight)
+})
+
+// Border Type //
+//Dot//
+let dotButton = document.getElementById("dotButton")
+
+function makeDottedBorder(element) {
+  element.style.borderStyle = "dotted"
+}
+dotButton.addEventListener("click", function(){
+  makeDottedBorder(border)
+});
+//Solid//
+let solidButton = document.getElementById("solidButton")
+
+function makeSolidBorder(element) {
+  element.style.borderStyle = "solid"
+}
+solidButton.addEventListener("click", function(){
+  makeSolidBorder(border)
+});
+
+// Move Circle //
+let moveX = document.getElementById("moveX")
+let moveY = document.getElementById("moveY")
+
+function changePosition(element, xInput, yInput) {
+  element.style.transform = `translate(${xInput.value}px, ${yInput.value}px)`
+}
+moveX.addEventListener("input", function (event) {
+  changePosition(circle, moveX, moveY);
+})
+moveY.addEventListener("input", function (event) {
+  changePosition(circle, moveX, moveY);
+})
